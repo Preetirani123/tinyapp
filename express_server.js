eb2a050cbd58c1e121fb0828e57f8a54d6e74eae
Preetirani123 <rani.preeti3333@gmail.com>
2021-02-17 03:10:27 +0000

    Tiny app with login and logout

--- express_server.js
+++ express_server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
+
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
+
+const cookieParser = require('cookie-parser')
+app.use(cookieParser());
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,33 +20,32 @@ const generateRandomString = function(number) {
   }
   return result;
 }
-//--------------------------------------------------------//
-// ------ Database ---------------------------------------
+//End//
+
+//Database//
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+//End//
 
-//------------ Home page/root-----------------------------
+//Display url_index page to displahy the current databse//
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase, username : req.cookies['username']};
   res.render("urls_index", templateVars);
 });
+//End//
 
-// ------------made short id = long id ---------------------
-app.post("/urls", (req, res) => {
-  let shortID = generateRandomString(6);
-  urlDatabase[shortID] = req.body['longURL'];
-  res.redirect(`/urls/${shortID}`);
-});
-
+//Create url_new page to create new URL//
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = {username : req.cookies['username']};
+  res.render("urls_new", templateVars);
 });
+//End//
 
+//Display individual URL pages//
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username : req.cookies['username']};
   if (templateVars.longURL) {
     res.render("urls_show", templateVars);
   } else {
@@ -50,31 +53,55 @@ app.get("/urls/:shortURL", (req, res) => {
     res.send("Page not found");
   }
 });
+//End//
 
+//Redirect to individual LongURL sites//
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL]
   res.redirect(longURL);
 });
+//End//
 
+// Generate short id and add it to the database along with its longURL//
+app.post("/urls", (req, res) => {
+  let shortID = generateRandomString(6);
+  urlDatabase[shortID] = req.body['longURL'];
+  res.redirect(`/urls/${shortID}`);
+});
+//End//
+
+//Delete a URL from the database//
 app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
 });
+//End//
 
-// app.post("/urls/:shortURL/edit", (req, res) => {
-//   let shortID = req.params.shortURL;
-//   res.redirect(`/urls/${shortID}`);
-// });
-
+//Update a URL in the database//
 app.post("/urls/:shortURL/update", (req, res) => {
-  console.log(req);
   let shortID = req.params.shortURL;
   urlDatabase[shortID] = req.body['longURL']
   res.redirect("/urls");
 });
+//End//
 
+//Login//
+app.post("/login", (req, res) => {
+  let userName = req.body['username'];
+  res.cookie('username', userName)
+  res.redirect("/urls");
+});
+//End//
+
+//Logout//
+app.post("/logout", (req, res) => {
+  res.clearCookie('username')
+  res.redirect("/urls");
+});
+//End//
+
+//Listening to the port//
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
-
-
+//End//
